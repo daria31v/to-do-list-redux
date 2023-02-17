9a1cf6df8e74486d67f5bc1065970207d21f76eb
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2023-02-17 20:19:05 +0100

    fix

--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -1,6 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { nanoid} from "nanoid";
-const tasksInitialState = [];
+import { nanoid } from "nanoid";
+
+const tasksInitialState = [
+  { id: 0, text: "Learn HTML and CSS", completed: true },
+  { id: 1, text: "Get good at JavaScript", completed: true },
+  { id: 2, text: "Master React", completed: false },
+  { id: 3, text: "Discover Redux", completed: false },
+  { id: 4, text: "Build amazing apps", completed: false },
+];
+
 
 const tasksSlice = createSlice({
   name: "tasks",
